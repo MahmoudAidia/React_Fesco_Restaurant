d39293293a56b39804a1260401bb5f3218120282
Mahmoud Mostafa Aidia <116117523+MahmoudAidia@users.noreply.github.com>
2024-05-13 19:55:24 +0300

    Finished footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,35 +5,44 @@ import OpenHours from "./OpenHours";
 import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 
 import styles from "./Footer.module.css";
+
 function Footer() {
   return (
-    <footer className={styles.footer}>
+    <footer>
       <Container>
-        <div className={styles.footerContainer}>
-          <div className={styles.footerCol}>
-            <Address />
-            <ReservationBtn>View Map</ReservationBtn>
-          </div>
-
-          <div className={styles.footerCol}>
-            <OpenHours />
-            <ReservationBtn>View Map</ReservationBtn>
-          </div>
-
-          <div className={styles.social}>
-            <div>
-              <h4>We are social</h4>
-              <p>Feel free to check our social media pages.</p>
+        <div className={styles.footer}>
+          <div className={styles.colContainer}>
+            <div className={styles.col1}>
+              <Address />
             </div>
-            <div className={styles.socialIcons}>
+            <ReservationBtn>View map</ReservationBtn>
+          </div>
+
+          <div className={styles.colContainer}>
+            <div className={styles.col2}>
+              <OpenHours />
+            </div>
+
+            <ReservationBtn>Table Reservation</ReservationBtn>
+          </div>
+
+          <div className={styles.colContainer}>
+            <div className={styles.col3}>
+              <h3>We are social:</h3>
+              <p>Feel free to check our social media</p>
+            </div>
+
+            <div className={styles.socialIconsContainer}>
               <div>
-                <FaFacebook />
+                <FaFacebook className={styles.icon} />
               </div>
+
               <div>
-                <FaInstagram />
+                <FaInstagram className={styles.icon} />
               </div>
+
               <div>
-                <FaTwitter />
+                <FaTwitter className={styles.icon} />
               </div>
             </div>
           </div>
@@ -44,3 +53,47 @@ function Footer() {
 }
 
 export default Footer;
+
+// function Footer() {
+//   return (
+//     <footer className={styles.footer}>
+//       <Container>
+//         <div className={styles.footerContainer}>
+//           <div className={styles.footerCol}>
+//             <div className={styles.footerBox}>
+//               <Address />
+//             </div>
+//             <ReservationBtn>View Map</ReservationBtn>
+//           </div>
+
+//           <div className={styles.footerCol}>
+//             <div className={styles.footerBox}>
+//               <OpenHours />
+//             </div>
+//             <ReservationBtn>View Map</ReservationBtn>
+//           </div>
+
+//           <div className={styles.social}>
+//             <div>
+//               <h4>We are social</h4>
+//               <p>Feel free to check our social media pages.</p>
+//             </div>
+//             <div className={styles.socialIcons}>
+//               <div>
+//                 <FaFacebook />
+//               </div>
+//               <div>
+//                 <FaInstagram />
+//               </div>
+//               <div>
+//                 <FaTwitter />
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </Container>
+//     </footer>
+//   );
+// }
+
+// export default Footer;
